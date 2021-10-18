233a46dcc4eb46d1a28a27697b45042ad76a7f3a
Jithin Thomas <74818824+jith47@users.noreply.github.com>
2021-10-18 13:54:41 +0530

    Update settings.py

--- src/kirr/settings.py
+++ src/kirr/settings.py
@@ -131,6 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+ # live cdn AWS S3
+
+
+
+
 
 SHORTCODE_MAX = 13
 SHORTCODE_MIN = 4
