61a3b5701bbdfb4267bd3064effb968b58a5f936
Jithin Thomas <74818824+jith47@users.noreply.github.com>
2021-10-19 18:05:24 +0530

    Update settings.py

--- src/kirr/settings.py
+++ src/kirr/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'kirr.urls'
 ROOT_HOSTCONF = 'kirr.hosts'
 DEFAULT_HOST = 'www'
-DEFAULT_REDIRECT_URL = "http://www.getshorturl.co"
-PARENT_HOST = "getshorturl.co"
+DEFAULT_REDIRECT_URL = "http://getshorturl.herokuapp.com"
+PARENT_HOST = "getshorturl.herokuapp.com"
 
 TEMPLATES = [
     {
